17b12fc334d6685a6e4b026c60fef6bd2d72bffa
Fabiano De Paula <fabiano.rpaula@gmail.com>
2022-09-13 20:32:39 -0300

    Animado e Movendo

---
+++ Assets/Scripts/Heroi.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroi : MonoBehaviour
+{
+    private Rigidbody2D Corpo;
+    private Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Corpo = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimento
+        float velX = Input.GetAxis("Horizontal");
+        Corpo.velocity = new Vector2(velX, 0);
+
+        if(velX == 0)
+        {
+            Anim.SetBool("Andar", false);
+        }
+        else
+        {
+            Anim.SetBool("Andar", true);
+        }
+
+
+    }
+}
