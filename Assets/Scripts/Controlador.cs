f93d6550aae2686c75d9527e8c717824d7149593
Fabiano De Paula <fabiano.rpaula@gmail.com>
2022-10-18 20:34:37 -0300

    Paralax

--- Assets/Scripts/Controlador.cs
+++ Assets/Scripts/Controlador.cs
@@ -9,7 +9,7 @@ public class Controlador : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 0;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
